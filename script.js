323f52141259ca7588ee70745b863515a1ccf6dd
Zeynep <zeynepmarch@gmail.com>
2022-09-21 14:02:08 +1000

    Add function that updates table with new todos

--- script.js
+++ script.js
@@ -1,20 +1,46 @@
 // factory for 'to-do' object with date, action to be done, status(complete)? priority
 
-const newToDo = (task, priority, dueDate) => {
+const toDoFactory = (task, priority, dueDate) => {
     const getCurrentDate = Intl.DateTimeFormat("en", {
-        dateStyle: 'short',
+        dateStyle: 'short',                                 //format date to have dd/mm/yy
     });
-    const date = getCurrentDate.format(Date.now());
+    const date = getCurrentDate.format(Date.now()); 
 
     
 
     return {
-        date,
         task,
-        priority,
         dueDate,
+        priority,
+        date
     }
 }
 
-newToDo();
+
+// Function to add to-dos to the DOM
+
+const table = document.querySelector('table');
+
+function addToDo (input) {  //Creates a new row containing the to-do information
+    const newTableRow = document.createElement('tr');
+    const toDoProperties = ['task', 'dueDate', 'priority', 'date'];       
+        for (let i = 0; i < 4; i++) {                           
+            const newTbCell = document.createElement('td');
+            newTbCell.textContent += input[toDoProperties[i]];
+            newTableRow.appendChild(newTbCell);
+        }
+    table.appendChild(newTableRow);
+    
+}
+
+function newToDo (task, priority, dueDate) {
+    const newTask = toDoFactory(task, priority, dueDate);
+    addToDo(newTask);
+    console.log(newTask);
+
+}
+
 // Categories for separate lists of to-dos
+
+
+// Local storage to remember a user's tasks
